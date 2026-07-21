c2aefcb0289c1397e434b80ff43fe204000df388
Erik Villarreal <erikvillarreal9505@gmail.com>
2026-07-20 20:32:00 -0500

    Configuración de GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://95xErvit.github.io",
+  base: "/portfolio",
+
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
