eb0837b3c9919151c57b5941e447a9331980ef39
Erik Villarreal <erikvillarreal9505@gmail.com>
2026-08-30 22:38:16 -0500

    UPDATE description Project.ts

--- src/data/project.ts
+++ src/data/project.ts
@@ -4,7 +4,7 @@ export const projects = [
     {
         id: 1,
         title: "Intranet Corporativa",
-        description: "Desarrollo web de actualización del portal de intranet para <strong>la Feria del brasier y solo kuko's</strong> para la gestión de información y procesos internos de la empresa, incluyendo módulo de administración y módulos por área de la compañia, para su comunicación y colaboración.",
+        description: "Rediseño UX/UI y desarrollo web para la actualización del portal de intranet para <strong>la Feria del brasier y solo kuko's</strong> para la gestión de información y procesos internos de la empresa, incluyendo módulo de administración y módulos por área de la compañia, para su comunicación y colaboración.",
         image: buildAssetUrl("/Intranet_FDB.png"),
         url: "https://intranet.feriadelbrasier.com.co/",
     },
@@ -12,7 +12,7 @@ export const projects = [
     {
         id: 2,
         title: "Sistema de Créditos",
-        description: "Desarrollo de plataforma web <strong>CrediFeria</strong> para <strong>la Feria del brasier y solo kuko's</strong>, para la gestión integral de créditos y autogestión de clientes, incluyendo solicitudes, aprobaciones y seguimiento. Con módulo de administración, indicadores en tiempo real y procesos digitales.",
+        description: "Diseño UX/UI y Desarrollo de plataforma web <strong>CrediFeria</strong> para <strong>la Feria del brasier y solo kuko's</strong>, para la gestión integral de créditos y autogestión de clientes, incluyendo solicitudes, aprobaciones y seguimiento. Con módulo de administración, indicadores en tiempo real y procesos digitales.",
         image: buildAssetUrl("/CrediFeria.png"),
         url: "https://creditos.feriadelbrasier.com.co/",
     },
@@ -20,7 +20,7 @@ export const projects = [
     {
         id: 3,
         title: "Sistema de PQRS",
-        description: "Desarrollo de plataforma Web de gestión de PQRS para <strong>la Feria del brasier y solo kuko's</strong>, con módulo de administración para la gestión del proyecto. Sistema de gestión de peticiones, quejas, reclamos y solicitudes, con notificaciones, seguimiento y reportes.",
+        description: "Diseño UX/UI y Desarrollo de plataforma Web de gestión de PQRS para <strong>la Feria del brasier y solo kuko's</strong>, con módulo de administración para la gestión del proyecto. Sistema de gestión de peticiones, quejas, reclamos y solicitudes, con notificaciones, seguimiento y reportes.",
         image: buildAssetUrl("/PQRS.png"),
         url: "https://pqrs.feriadelbrasier.com.co/Client",
     },
